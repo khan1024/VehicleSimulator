b4e5634f5f05a4b8e29af862451d406dc49c5503
Ryan Warrick <ryanwarrick1@gmail.com>
2014-10-23 17:06:53 -0700

    Created first file and put in simple comments for start of project.

---
+++ Main.py
@@ -0,0 +1,11 @@
+'''
+Author: Ryan Warrick
+Group: OSUSVT
+File: Main.py
+Description: The Vehicle Simulator Program for strategy purposes intended to be used by the OSUSVT.
+Github Repo: https://github.com/ryanwarrick/VehicleSimulator
+'''
+
+__author__ = 'Ryan'
+
+print("OSUSVT Vehicle Simulator")
